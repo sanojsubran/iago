fe74d1bdeacf557e2a917eef0f20062ec24771ef
sanojsubran <sanoj009@gmail.com>
2023-05-01 21:08:16 +0200

    embedded the config, refactored the news handler

--- main.go
+++ main.go
@@ -1,42 +1,28 @@
 package main
 
 import (
+	_ "embed"
 	"log"
 	"net/http"
 	"time"
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
-	"github.com/knadh/koanf"
-	"github.com/knadh/koanf/parsers/json"
-	"github.com/knadh/koanf/providers/file"
 	"github.com/sirupsen/logrus"
 
 	iago "github.com/sanojsubran/iago/pkg/iago"
 )
 
-// Global koanf instance. Use "." as the key path delimiter. This can be "/" or any character.
-var k = koanf.New(".")
-
-func readConfig() {
-	// Load JSON config.s
-	if err := k.Load(file.Provider("./config.json"), json.Parser()); err != nil {
-		log.Fatalf("error loading config: %v", err)
-	}
-}
+//go:embed config.json
+var cfg string
 
 func main() {
-	//TODO To be implemented later
-	//readConfig()
-
-	sources := make([]iago.NewsSource, 0)
-
-	//TODO: populate the sources with configured news sources
-
 	r := mux.NewRouter()
 	var news iago.NewsHandler
 
 	news.Init()
+
+	sources := news.GetConfiguredSources(cfg)
 
 	go func() {
 		for {
@@ -53,7 +39,7 @@ func main() {
 	header := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
 	methods := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS"})
 	origins := handlers.AllowedOrigins([]string{"*"})
-	r.HandleFunc("/", news.HandleNewsReq)
+	r.HandleFunc("/", news.HandleNewsRequests)
 	log.Fatal(http.ListenAndServe(":8081", handlers.CORS(header, methods, origins)(r)))
 
 }
--- pkg/iago/newshandler.go
+++ pkg/iago/newshandler.go
@@ -9,9 +9,23 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+// Config defines settings of app. Currently, the embedded file provides details of the news sources.
+type Config struct {
+	Sources []NewsSource `json:"sources"`
+}
+
+// NewsHandler defines the type which stores and handles the feed data.
 type NewsHandler struct {
 	mutex       *sync.Mutex
 	newsContent map[string][]story
+}
+
+// NewsSource defines the type which stores the details of a news source.
+type NewsSource struct {
+	Url        string `json:"url"`
+	ClientType string `json:"clientType"`
+	Topic      string `json:"topic"`
+	EntryCount int    `json:"entryCount"`
 }
 
 // feedContent represents the complete feed data from a single source.
@@ -28,16 +42,65 @@ type story struct {
 	Url   string `json:"url"`
 }
 
-type NewsSource struct {
-	Url        string
-	ClientType string
-	Topic      string
-	EntryCount int
-}
-
+// Init initializes the newsHandler struct.
 func (n *NewsHandler) Init() {
 	n.mutex = &sync.Mutex{}
 	n.newsContent = make(map[string][]story)
+}
+
+// GetConfiguredSources gets the news sources from the config data.
+func (n *NewsHandler) GetConfiguredSources(config string) []NewsSource {
+
+	var configData Config
+
+	err := json.Unmarshal([]byte(config), &configData)
+
+	if err != nil {
+		logrus.Error("unable to parse the config data. Error: ", err.Error())
+	}
+
+	sources := configData.Sources
+
+	if len(sources) == 0 {
+		logrus.Error("count of parsed sources is zero. Check the config.json.")
+	}
+
+	return sources
+}
+
+// UpdateFeed updates the news feed data content.
+func (n *NewsHandler) UpdateFeed(s NewsSource) error {
+	readClient := n.getSourceHandler(s)
+
+	if readClient == nil {
+		return errors.New("unable to identify the client reader for the news source")
+	}
+
+	d, err := readClient.readData()
+
+	if err != nil {
+		logrus.Error("unable to fetch the from the news source. Error: " + err.Error())
+		return err
+	}
+
+	n.updateNewsFeed(d.Title, d.Article)
+
+	return nil
+}
+
+// HandleNewsRequests handles the incoming requests.
+func (n *NewsHandler) HandleNewsRequests(w http.ResponseWriter, _ *http.Request) {
+	logrus.Info("received a request. Processing...")
+
+	data := n.generateNewsStream()
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+
+	w.WriteHeader(http.StatusOK)
+	if wC, err := w.Write(data); err != nil {
+		logrus.Errorf("\nerror while writing response. error: %s, write count: %d", err.Error(), wC)
+	}
 }
 
 // generateNewsStream generates the data to be sent via HTTP from the news content map.
@@ -64,39 +127,6 @@ func (n *NewsHandler) getSourceHandler(s NewsSource) fetcher {
 		return hnClient{name: s.Topic, count: s.EntryCount}
 	}
 	return nil
-}
-
-func (n *NewsHandler) UpdateFeed(s NewsSource) error {
-	readClient := n.getSourceHandler(s)
-
-	if readClient == nil {
-		return errors.New("unable to identify the client reader for the news source")
-	}
-
-	d, err := readClient.readData()
-
-	if err != nil {
-		logrus.Error("unable to fetch the from the news source. Error: " + err.Error())
-		return err
-	}
-
-	n.updateNewsFeed(d.Title, d.Article)
-
-	return nil
-}
-
-func (n *NewsHandler) HandleNewsReq(w http.ResponseWriter, r *http.Request) {
-	logrus.Info("received a request. Processing...")
-
-	data := n.generateNewsStream()
-
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Credentials", "true")
-
-	w.WriteHeader(http.StatusOK)
-	if wC, err := w.Write(data); err != nil {
-		logrus.Errorf("\nerror while writing response. error: %s, write count: %d", err.Error(), wC)
-	}
 }
 
 func (n *NewsHandler) updateNewsFeed(src string, data []story) {
